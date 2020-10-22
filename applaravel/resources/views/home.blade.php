db1370071f50ba74a2489fb1f3cf743a1574c543
marcoweb <marcocarvalho.web@hotmail.com>
2020-10-22 11:52:22 +0000

    Exemplo de Rotas

---
+++ applaravel/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <title>Exemplo</title>
+    </head>
+    <body>
+        <h1>Exemplo de View</h1>
+    </body>
+</html>
